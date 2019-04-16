f1ac2200d983423dcdcc14c962713fab5bb446ff
Sam Hambridge <samhambridge@hotmail.com>
2019-04-16 17:38:49 +0100

    commit

--- js/jquery.js
+++ js/jquery.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // smooth scrolling initializaion
 (function jqueryIIFE() {
     $("a").on("click", function (e) {
@@ -18,25 +17,4 @@
             );
         }
     });
-=======
-// smooth scrolling initializaion
-(function jqueryIIFE() {
-    $("a").on("click", function (e) {
-        if (this.hash !== "") {
-            e.preventDefault();
-
-            let hash = this.hash;
-
-            $("html, body").animate(
-                {
-                    scrollTop: $(hash).offset().top
-                },
-                800,
-                () => {
-                    window.location.hash = hash;
-                }
-            );
-        }
-    });
->>>>>>> 4e72687ef71d61d5eb637dd819d9359750db03d6
 }())
